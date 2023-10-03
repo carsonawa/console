a29c49008067b49606b38bc9c127e46a646cf394
carsonawa <138302207+carsonawa@users.noreply.github.com>
2023-10-03 11:38:28 +0800

    Update console.py

--- console.py
+++ console.py
@@ -30,23 +30,3 @@ def consoleList():
 def writeLog(file) :
     with open(file, "w") as f:
         f.write(consoleList())
-
-if (__name__ == '__main__'):
-    print('''
-帮助:
-          
-变量：
-consoles\t以列表形式引入控制台日志
-
-函数：
-cout\t\t日志打印
-warn\t\t警告打印
-error\t\t报错打印
-console\t\t打印控制台日志
-consoleList\t以字符串形式返回控制台日志
-writeLog\t将日志内容写入到文件里
-
-注：如果是直接import引入，需要加“console.”
-    ''')
-    input('按回车继续……')
-    #print("示例：")
